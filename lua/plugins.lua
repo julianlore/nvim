e2852473175f76fbeddede878444449d3debaef1
Julian Lore <julian.lore@mail.mcgill.ca>
2023-06-16 18:38:07 -0400

    Move all plugins to their own file

---
+++ lua/plugins.lua
@@ -0,0 +1,171 @@
+return {
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  { -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+
+  { -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Fuzzy Finder (files, lsp, etc)
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  -- Only load if `make` is available. Make sure you have the system
+  -- requirements installed.
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    -- NOTE: If you are having trouble with this installation,
+    --       refer to the README for telescope-fzf-native for more instructions.
+    build = 'make',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ":TSUpdate",
+  },
+  {
+    'johnfrankmorgan/whitespace.nvim',
+        config = function()
+          require('whitespace-nvim').setup({
+          -- configuration options and their defaults
+
+          -- `highlight` configures which highlight is used to display
+          -- trailing whitespace
+          highlight = 'DiffDelete',
+
+          -- `ignored_filetypes` configures which filetypes to ignore when
+          -- displaying trailing whitespace
+          ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+          })
+          -- remove trailing whitespace with a keybinding
+          vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim)
+        end,
+  },
+  {
+    'ggandor/leap.nvim',
+      config = function()
+        require('leap').add_default_mappings()
+      end,
+  },
+  {
+    'windwp/nvim-autopairs',
+      config = function ()
+        require('nvim-autopairs').setup {}
+      end
+  },
+  {
+    'HiPhish/nvim-ts-rainbow2'
+  },
+  {
+  "smartpde/telescope-recent-files",
+  config = function()
+    require("telescope").load_extension("recent_files")
+  end,
+  },
+  { 'jose-elias-alvarez/null-ls.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'renerocksai/telekasten.nvim',
+      config = function ()
+        require('telekasten').setup({
+          home = os.getenv("TELEKASTEN_DIR"),
+          dailies = os.getenv("TELEKASTEN_DAILIES_DIR"),
+          auto_set_filetype = false,
+          dailies_create_nonexisting = false,
+          subdirs_in_links = false,
+      })
+      end,
+      dependencies = { 'nvim-telescope/telescope.nvim' }
+  },
+  { 'ixru/nvim-markdown' },
+  { 'ahmedkhalf/project.nvim',
+      config = function()
+        require('project_nvim').setup {
+        require('telescope').load_extension('projects')
+      }
+      end
+  },
+  { 'vladdoster/remember.nvim',
+      config = function()
+        require('remember').setup {}
+      end
+  },
+}
