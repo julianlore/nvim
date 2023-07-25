ac5b574bce6e1069d163f03d76fae8714b661557
Julian Lore <julian.lore@mail.mcgill.ca>
2023-07-24 22:16:42 -0400

    Add more explicit Telekasten insert link keymap Facilitates adding links to non existent files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,3 @@
 -- Call insert link automatically when we start typing a link for markdown buffers only
-vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>", { buffer = true } )
+vim.keymap.set("i", "i[", "<cmd>Telekasten insert_link<CR>", { buffer = true } )
+vim.keymap.set("i", "p[", "<cmd>Telekasten insert_link<CR>", { buffer = true } )
